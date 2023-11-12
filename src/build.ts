c0e83fb6f472a9f64d257acd552cdc5b854bf1e4
Koki Takahashi <hakatasiloving@gmail.com>
2023-11-12 22:54:37 +0000

    Update build.ts

--- src/build.ts
+++ src/build.ts
@@ -2,8 +2,7 @@ import yaml from 'js-yaml';
 import fs from 'fs/promises';
 import path from 'path';
 import {fileURLToPath} from 'url';
-// @ts-expect-error: Untyped
-import Ajv from 'ajv/dist/2020';
+import Ajv from 'ajv/dist/2020.js';
 import {compile as compileToTs} from 'json-schema-to-typescript'
 
 const toAbsolute = (p: string) => path.resolve(fileURLToPath(import.meta.url), '..', p);
@@ -16,6 +15,7 @@ await fs.writeFile(toAbsolute('condition.schema.json'), JSON.stringify(condition
 const ts = await compileToTs(conditionSchema, 'Condition');
 await fs.writeFile(toAbsolute('condition.d.ts'), ts);
 
+// @ts-expect-error: ???
 const validate = new Ajv().compile(conditionSchema);
 
 console.log(validate.errors);
